9b1d308f1bbdddc65fb1b68152fbf9acd12b4f3c
Lena Palasionak <19lenka87@tut.by>
2022-03-16 12:35:15 +0300

    Dependency Injection.init-, destroy-methods for Prototypes(only init)

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,25 +9,31 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
+        //init-method будет вызван здесь, до создания объекта
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+//
+//        MusicPlayer firstPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        /* Сейас будет проверка Singleton'a */
+//
+//        boolean comparison = firstPlayer == secondPlayer;
+//        System.out.println(comparison);
+//        System.out.println(firstPlayer);
+//        System.out.println(secondPlayer);
+//
+//        firstPlayer.setVolume(10);
+//        System.out.println(firstPlayer.getVolume());
+//        System.out.println(secondPlayer.getVolume());
+//
+//        firstPlayer.playMusic();
+//        System.out.println(firstPlayer.getName());
+//        System.out.println(firstPlayer.getVolume());
 
-        MusicPlayer firstPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        //когда метод мэйн завершится - будет вызван destroy-method
+        System.out.println("контекст еще не закрыт");
+        context.close();// а потом закрыт контекст
 
-        /* Сейас будет проверка Singleton'a */
-
-        boolean comparison = firstPlayer == secondPlayer;
-        System.out.println(comparison);
-        System.out.println(firstPlayer);
-        System.out.println(secondPlayer);
-
-        firstPlayer.setVolume(10);
-        System.out.println(firstPlayer.getVolume());
-        System.out.println(secondPlayer.getVolume());
-
-        firstPlayer.playMusic();
-        System.out.println(firstPlayer.getName());
-        System.out.println(firstPlayer.getVolume());
-
-        context.close();
     }
 }
