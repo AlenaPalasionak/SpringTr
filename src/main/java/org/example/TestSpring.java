cbb74cf0976acd1c9e93dbd54aabe70b68708726
Lena Palasionak <19lenka87@tut.by>
2022-03-16 11:43:37 +0300

    Dependency Injection.Singleton

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -10,11 +10,23 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        /* Сейас будет проверка Singleton'a */
+
+        boolean comparison = firstPlayer == secondPlayer;
+        System.out.println(comparison);
+        System.out.println(firstPlayer);
+        System.out.println(secondPlayer);
+
+        firstPlayer.setVolume(10);
+        System.out.println(firstPlayer.getVolume());
+        System.out.println(secondPlayer.getVolume());
+
+        firstPlayer.playMusic();
+        System.out.println(firstPlayer.getName());
+        System.out.println(firstPlayer.getVolume());
 
         context.close();
     }
