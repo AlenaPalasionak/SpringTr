cf0baa1da2b4336301c233a6e940dca2896c1c2e
Lena Palasionak <19lenka87@tut.by>
2022-03-17 11:18:57 +0300

    Dependency Injection.Annotation Component.

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component //если мы помечаем класс этой аннотацией, то спринг создаст нам бины этих классов, а значит и объекты
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -2,20 +2,29 @@ package org.example;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * @author Neil Alishev
- */
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-
+        Music music = context.getBean("rockMusic", Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(music);
-
         musicPlayer.playMusic();
+
+        Music music2 = context.getBean("classicalMusic", Music.class);
+        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+        musicPlayer2.playMusic();
 
         context.close();
     }
 }
+/*
+Algorithm:
+1. <context:component-scan base-package="org.example"/> in applicationContecst
+2. mark the classes with annotation to say spring that it should create beans
+3. ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"); - open the Bean container
+4. Music music = context.getBean("rockMusic", Music.class); - get the Bean
+5. MusicPlayer musicPlayer = new MusicPlayer(music); ctreate the object and put the beanObject into a parameters
+6.use the method of the object
+ */
