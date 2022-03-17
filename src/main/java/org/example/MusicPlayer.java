4dde122bde374a1311ffc9ca5aed6743cc578c0c
Lena Palasionak <19lenka87@tut.by>
2022-03-17 12:38:33 +0300

    Dependency Injection.Annotation Autowired.two dependencies(two arguments in constructor.)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -5,11 +5,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    @Autowired //use reflection - not recommended by idea
-    private Music music;
 
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+    }
 
     public void playMusic()  {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + classicalMusic.getSong());
+        System.out.println("Playing: " + rockMusic.getSong());
     }
 }
