a3093c4b6de4dc4f5be1924fd27a6ae899db677c
Lena Palasionak <19lenka87@tut.by>
2022-03-17 12:07:56 +0300

    Dependency Injection.Annotation Autowired

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,24 +7,22 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
+MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+musicPlayer.playMusic();
 
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-        musicPlayer2.playMusic();
+        /* Algorithm
+        1.сканируются все классы с аннотацией @Component - созд. бины
+        2. спринг проверяет подходят ли бины в качестве зависимости там, где мы указали @Autowired
+         */
 
-        context.close();
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//        musicPlayer2.playMusic();
+//
+//        context.close();
     }
 }
-/*
-Algorithm:
-1. <context:component-scan base-package="org.example"/> in applicationContecst
-2. mark the classes with annotation to say spring that it should create beans
-3. ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"); - open the Bean container
-4. Music music = context.getBean("rockMusic", Music.class); - get the Bean
-5. MusicPlayer musicPlayer = new MusicPlayer(music); ctreate the object and put the beanObject into a parameters
-6.use the method of the object
- */
