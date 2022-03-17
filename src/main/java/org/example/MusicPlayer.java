9ff162c6753b5ed9a58a5803e099b46e16cce2a6
Lena Palasionak <19lenka87@tut.by>
2022-03-17 13:22:20 +0300

    Dependency Injection.Annotation Qualifier through the field.

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,22 +1,16 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
-public class MusicPlayer {
+    @Component
+    public class MusicPlayer {
+        @Autowired
+        @Qualifier("classicalMusic")
+        private Music music;
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+        public void playMusic()  {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
-
-    public void playMusic()  {
-        System.out.println("Playing: " + classicalMusic.getSong());
-        System.out.println("Playing: " + rockMusic.getSong());
-    }
-}
