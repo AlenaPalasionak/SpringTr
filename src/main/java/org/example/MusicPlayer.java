0f8347cd059c9eb19e26ca912e7ffb2faea20d95
Lena Palasionak <19lenka87@tut.by>
2022-03-17 12:33:29 +0300

    Dependency Injection.Annotation Autowired wired through the field(using reflection).

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -5,15 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    @Autowired //use reflection - not recommended by idea
     private Music music;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-@Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
 
     public void playMusic()  {
         System.out.println("Playing: " + music.getSong());
