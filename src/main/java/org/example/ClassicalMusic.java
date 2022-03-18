8e9213bf9fd375faaba98a3d47a3e3fdde80c1e2
Lena Palasionak <19lenka87@tut.by>
2022-03-18 12:14:05 +0300

    DI. manual @Bean creating in Java(@PreDestroy - work in this code)

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -2,8 +2,22 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
-@Component //если мы помечаем класс этой аннотацией, то спринг создаст нам бины этих классов, а значит и объекты
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    // Для Prototype бинов не вызывается destroy-метод!
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,25 +1,24 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
+        );
 
-Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
-//MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//musicPlayer.playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-//        Music music = context.getBean("rockMusic", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-//        musicPlayer.playMusic();
-//
-//        Music music2 = context.getBean("classicalMusic", Music.class);
-//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-//        musicPlayer2.playMusic();
-//
-//        context.close();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+
+        System.out.println(classicalMusic1 == classicalMusic2);
+
+        context.close();
     }
 }
